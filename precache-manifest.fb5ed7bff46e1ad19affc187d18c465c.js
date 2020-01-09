a6cd615b8682b6fa4d8718353bde1ea7a91d6da8
Claus Juul <clausjuul_@live.dk>
2020-01-09 15:47:50 +0100

    Updates

---
+++ precache-manifest.fb5ed7bff46e1ad19affc187d18c465c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c5312be8594bfd009b59bc943dd9a6",
+    "url": "/projectum-case/index.html"
+  },
+  {
+    "revision": "e7e10086c7f770932232",
+    "url": "/projectum-case/static/css/main.25cacd81.chunk.css"
+  },
+  {
+    "revision": "8c48c6d36094ddccc58e",
+    "url": "/projectum-case/static/js/2.6663d7e1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/projectum-case/static/js/2.6663d7e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7e10086c7f770932232",
+    "url": "/projectum-case/static/js/main.9ca7dd5a.chunk.js"
+  },
+  {
+    "revision": "62596da2838e8e606ea6",
+    "url": "/projectum-case/static/js/runtime-main.a87e8580.js"
+  }
+]);
