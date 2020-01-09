840f87690e011000be68772e0b77d3526048de94
Claus Juul <clausjuul_@live.dk>
2020-01-09 17:41:48 +0100

    Updates

--- precache-manifest.f42dcf087f9d92ad29f55207a1fd43aa.js
+++ precache-manifest.f42dcf087f9d92ad29f55207a1fd43aa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a65ff70660350167ca2424e119d2b37",
+    "revision": "afe546ef763609984a15abf0ff0c0eaf",
     "url": "/projectum-case/index.html"
   },
   {
-    "revision": "2e43c9c615d736f08c0f",
+    "revision": "30ef84436a7a57a00b98",
     "url": "/projectum-case/static/css/main.4c55bce5.chunk.css"
   },
   {
-    "revision": "7a33c7117d161adc005c",
-    "url": "/projectum-case/static/js/2.142bcd25.chunk.js"
+    "revision": "5911a78ba33715c03186",
+    "url": "/projectum-case/static/js/2.f81748f5.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/projectum-case/static/js/2.142bcd25.chunk.js.LICENSE"
+    "url": "/projectum-case/static/js/2.f81748f5.chunk.js.LICENSE"
   },
   {
-    "revision": "2e43c9c615d736f08c0f",
-    "url": "/projectum-case/static/js/main.1f8f12e8.chunk.js"
+    "revision": "30ef84436a7a57a00b98",
+    "url": "/projectum-case/static/js/main.a25f0255.chunk.js"
   },
   {
     "revision": "62596da2838e8e606ea6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projectum-case/precache-manifest.750beb9b762a24c0cd0041f7d6e5e330.js"
+  "/projectum-case/precache-manifest.f42dcf087f9d92ad29f55207a1fd43aa.js"
 );
 
 self.addEventListener('message', (event) => {
